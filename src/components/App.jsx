ba47d6d961927c9f81e57dd338ba4831a6317ad6
Stanislav Kintoriak <kintoriak@gmail.com>
2023-05-06 23:58:56 +0300

    commit 1
    1) added basic structure: modules and css files;
    2) installed nanoid package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+
+export class App extends Component {
+
+
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+
+render(){
+
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
+
+
+
+}
 };
